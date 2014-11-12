f6272c2343493129dc3abda42054ea3cdccd59f2
Heartless <heartless@heartless-355V4C-356V4C-3445VC-3545VC>
2014-11-12 13:33:33 +0400

    Fix

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -9,10 +9,8 @@
 	  	});
 	  	$('.repair .item.active .pricing .content li.price_id').each(function(i) {
 	  		var fix = i % heightPrice.length; 
-
-	    	$(this).height(heightPrice[fix]);
-	    	$(this).css('height', heightPrice[fix] - (heightPrice[fix] / 2 - 10));
-				$(this).css('margin-top', (heightPrice[fix] / 2 - 10));
+	  		
+	    	$(this).css('min-height', heightPrice[fix]);
 	  	});
   	}
   	fix();
