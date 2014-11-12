d90b0fc563165eb3e4bc0c4738b3d93fc0dbdc7f
Heartless <heartless@heartless-355V4C-356V4C-3445VC-3545VC>
2014-11-12 12:10:55 +0400

    Fix

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -10,8 +10,7 @@
 	  	$('.repair .item.active .pricing .content li.price_id').each(function(i) {
 	  		var fix = i % heightPrice.length; 
 
-	    	$(this).height(heightPrice[fix] - (heightPrice[fix] / 2 - 10));
-	    	$(this).css('margin-top', (heightPrice[fix] / 2 - 10));
+	    	$(this).height(heightPrice[fix]);
 	  	});
   	}
   	fix();
