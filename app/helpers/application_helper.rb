d6d6e65be9f44cee5d3a553f05f4f7c47cee349d
Heartless <heartless@heartless-355V4C-356V4C-3445VC-3545VC>
2014-11-12 15:59:37 +0400

    Fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "Сервисный центр Инфоринг в Ульяновске"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+  
 	def nav_link(link_text, link_path)
 	  class_name = current_page?(link_path) ? 'active' : ''
 
